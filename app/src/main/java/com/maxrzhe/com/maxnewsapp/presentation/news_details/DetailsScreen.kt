30e9b9f7b60f921e5d8b0a21dd03e00491b9c7a7
mrzhevutski <mrzhevutski@suvoda.com>
2022-10-11 16:07:33 +0300

    add open url intent

--- app/src/main/java/com/maxrzhe/com/maxnewsapp/presentation/news_details/DetailsScreen.kt
+++ app/src/main/java/com/maxrzhe/com/maxnewsapp/presentation/news_details/DetailsScreen.kt
@@ -1,6 +1,5 @@
 package com.maxrzhe.com.maxnewsapp.presentation.news_details
 
-import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Surface
@@ -9,16 +8,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.maxrzhe.com.maxnewsapp.presentation.TAG
 
 @Composable
 fun DetailsScreen(
     viewModel: DetailsViewModel = hiltViewModel()
 ) {
     val state = viewModel.state
+    val uriHandler = LocalUriHandler.current
 
     Surface() {
         Column(
@@ -50,7 +50,7 @@ fun DetailsScreen(
                     } else {
                         state.articleModel?.let { article ->
                             DetailsItem(article = article, onGoToSiteClick = {
-                                Log.w(TAG, "DetailsScreen: url = $it")
+                                uriHandler.openUri(it)
                             })
                         }
                     }
